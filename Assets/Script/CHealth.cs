9b758c73e269ef292c217ef7b2b188208d43d043
AexZimmer <alxzimmer.zimmer1@gmail.com>
2021-02-08 23:46:47 -0300

    LogicPrograming

---
+++ Assets/Script/CHealth.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class CHealth : ScriptableObject
+{
+   public int Value
+    {
+        get { return _Value;  }
+    }
+
+    public int CurrentVaue
+    {
+        get { return _CurrentValue; }
+    }
+
+    [SerializeField]
+    public int _CurrentValue;
+
+    [SerializeField]
+    public int _Value;
+
+    public void init()
+    {
+        _CurrentValue = _Value;
+    }
+
+   public int Damage(int value)
+    {
+        _CurrentValue -= value;
+
+        return _CurrentValue;
+    }
+
+    public int heal(int value)
+    {
+        _CurrentValue += value;
+        return _CurrentValue;
+    }
+}
---
+++ Assets/Script/CHealthController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using System;
+public class CHealthController : MonoBehaviour
+{
+
+    public UnityEvent OnHealthChange;
+
+    public UnityEvent OnDeath;
+
+    [SerializeField]
+    private CHealth _health;
+
+
+
+    // Start is called before the first frame update
+    void Start() => _health.init();
+
+    private void OnTriggerEnter(Collider other)
+    {
+        var currentHealth = _health.Damage(2);
+        OnHealthChange.Invoke();
+
+        if(currentHealth < 1)
+        {
+            OnDeath.Invoke();
+        }
+
+    }
+
+    // Update is called once per frame
+    
+}
